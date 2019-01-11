e845eef027bd9aa0a6b3a10fbee753eeb006123b
Alexander Birukov <sanya.rnd@gmail.com>
2019-01-11 20:59:21 +0300

    Update disable_f1_hotkey.bat

--- disable_f1_hotkey.bat
+++ disable_f1_hotkey.bat
@@ -1,5 +1,13 @@
 @echo off
+
+REM kill the process just in case
 taskkill /f /im HelpPane.exe
+
+REM take ownership
 takeown /f %WinDir%\HelpPane.exe
-REM в зависимости от локали будет Everyone или Все
+
+REM group names depend on locale, i.e. "Everyone", "Все" etc
 icacls %WinDir%\HelpPane.exe /deny Everyone:(X)
+
+pause
+echo "Press any key to exit..."
